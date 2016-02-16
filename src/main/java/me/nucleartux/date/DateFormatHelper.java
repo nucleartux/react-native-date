ae6b4411ae37f19552d39ea12a869e4dab0dc6ed
Ara Israelyan <juggle@juggle.ru>
2016-02-16 15:43:45 +0300

    Update DateFormatHelper.java

--- src/main/java/me/nucleartux/date/DateFormatHelper.java
+++ src/main/java/me/nucleartux/date/DateFormatHelper.java
@@ -6,17 +6,35 @@ import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
 public class DateFormatHelper {
-  public static Calendar parseDate(String date, String dateTimeFormat) {
+  public static Calendar parseDate(String date) {
     Calendar initialDate = Calendar.getInstance();
 
     if (date != null && !date.equals(""))
     {
       try
       {
-        SimpleDateFormat sdf = new SimpleDateFormat(dateTimeFormat);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
         initialDate.setTime(sdf.parse(date));
       }
       catch (ParseException e)
+      {
+        return Calendar.getInstance();
+      }
+    }
+
+    return initialDate;
+  }
+
+  public static Calendar parseDateInMilliseconds(long ms) {
+    Calendar initialDate = Calendar.getInstance();
+
+    if (ms > 0)
+    {
+      try
+      {
+        initialDate.setTimeInMillis(ms);
+      }
+      catch (Exception e)
       {
         return Calendar.getInstance();
       }
